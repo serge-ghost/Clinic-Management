c92f7510611dc5d061109b322f989bbe0a4e7f0f
Serge <sharutyunyan@ghost-services.com>
2024-08-07 21:24:48 +0400

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'EXAMPLE FOR APPS',
+    'version': '1.0.1',
+    'website': 'https://www.erpswiss.com',
+    'images': ['static/description/localization15.gif'],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
+
